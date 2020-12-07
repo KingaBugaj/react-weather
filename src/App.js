25140b706d3e4254daea5adc4981dc74b9673d59
KingaBugaj <70944704+KingaBugaj@users.noreply.github.com>
2020-12-07 13:42:43 +0000

    GitHb repository added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        <a href="https://github.com/KingaBugaj/react-weather" target="_blank">
+          Open sourced
+        </a>{" "}
+        code by{" "}
+        <a
+          href="https://www.linkedin.com/in/kinga-bugaj-9172a7135/"
+          target="_blank"
+        >
+          Kinga Bugaj
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
